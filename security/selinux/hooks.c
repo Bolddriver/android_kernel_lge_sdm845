7ff51e09c6db196f904163806171c71d21d2c779
mirukurusan <86687241+mirukurusan@users.noreply.github.com>
2026-02-28 10:59:48 +0800

    scope-minimized manual hooks
    Change-Id: Ice26c0cc74370802f51f87c1740b95f69e1db820

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2317,6 +2317,10 @@ static u32 ptrace_parent_sid(struct task_struct *task)
 	return sid;
 }
 
+#ifdef CONFIG_KSU
+extern bool is_ksu_transition(const struct task_security_struct *old_tsec,
+			      const struct task_security_struct *new_tsec);
+#endif
 static int check_nnp_nosuid(const struct linux_binprm *bprm,
 			    const struct task_security_struct *old_tsec,
 			    const struct task_security_struct *new_tsec)
@@ -2351,6 +2355,11 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 			return 0;
 		}
 	}
+#endif
+
+#ifdef CONFIG_KSU
+	if (is_ksu_transition(old_tsec, new_tsec))
+		return 0;
 #endif
 
 	/*
